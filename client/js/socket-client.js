2ae4cf3c7c3fb9bfacc062b7c16e88810c8bf2a5
Josh <jhellerstein96@gmail.com>
2016-09-17 23:44:42 -0400

    fixing socket

--- client/js/socket-client.js
+++ client/js/socket-client.js
@@ -1,6 +1,20 @@
 'use strict';
-var socket = io('http://localhost:3000');
+var socket = io('http://dronar-app.herokuapp.com');
 
+socket.on('ADD_VIEW', (data) => {
+    $("#current").html(data.type);
+    $("#x").attr("placeholder","X position = " + data.x);
+    $("#y").attr("placeholder","Y position = " + data.y)
+    $("#z").attr("placeholder","Z position = " + data.z)
+    $("#zoom").attr("placeholder","Zoom = " + data.zoom)
+    $("#rotate").attr("placeholder","Rotate Degrees = " + data.rotate)
+    if(data.type === "graph") {
+        $("#equation").css("display","block");
+        $("#equation").attr("placeholder", "Current Equation: " + data.equation);
+    } else {
+        $("#equation").css("display","none");
+    }
+});
 /**
  * Serves an image
  */
@@ -12,17 +26,10 @@ function sendSelection(choice, eq) {
   socket.emit('SEL', {choice: parseChoice(choice), eq: eq});
 }
 
+function updateSelection(choice, x, y, z, zoom, rotate, equation) {
+    socket.emit('UPD', {choice: parseChoice(choice), x: x, y:y, z:z, zoom:zoom, rotate:rotate, equation:equation});
+}
+
 function parseChoice(choice) {
-  switch(choice) {
-    case 1:
-      return "skull";
-    case 2:
-      return "glucose";
-    case 3:
-      return "graph";
-    case 4:
-      return "physics2";
-    case 5:
-      return "physics";
-  }
+  return choice - 1;
 }     
