80a0af40e3e67bcb31276b79f8a24baef4a8c1d3
sebLopezCot <sclopez2@hotmail.com>
2016-09-17 21:04:04 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import request
+from urlparse import urlparse
+import requests
+import json
+import urllib2
+from urllib2 import urlopen
+from cookielib import CookieJar
+app = Flask(__name__)
+
+def check_if_empty(elements):
+	if len(elements) < 1:
+		return ''
+	else:
+		return elements[0]
+
+@app.route("/", methods=['GET'])
+def index():
+	if request.method == 'GET':
+		# Get incoming image url to reverse lookup
+		return 'G.G.nore'
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
